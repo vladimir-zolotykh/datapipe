75a13aea3f3fdf8f8dfa2b57961efde7f581b95e
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-03-13 21:09:18 +0200

    Add open_files_iter()

--- sift.py
+++ sift.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from typing import Iterator, IO
-
+from typing import Iterator
+from typing import Union, TextIO
 import os
 import fnmatch
+import gzip
+import bz2
+
+OPEN_IO = Union[gzip.GzipFile, bz2.BZ2File, TextIO]
 
 
 def files_iter(top: str, pat: str) -> Iterator[str]:
@@ -13,8 +17,17 @@ def files_iter(top: str, pat: str) -> Iterator[str]:
             yield os.path.join(path, fn)
 
 
-# def iter_open(filenames: Iterator[str]) -> Iterator[IO[str | bytes]]:
-#     yield f
+def open_files_iter(names: Iterator[str]) -> Iterator[OPEN_IO]:
+    for fn in names:
+        fo: OPEN_IO
+        if fn.endswith(".gz"):
+            fo = gzip.open(fn, "rb")
+        elif fn.endswith(".bz2"):
+            fo = bz2.open(fn, "rb")
+        else:
+            fo = open(fn, "rt")
+        yield fo
+        fo.close()
 
 
 # def iter_chain():
@@ -23,7 +36,7 @@ def files_iter(top: str, pat: str) -> Iterator[str]:
 
 
 # def iter_lines(
-#     open_files: Iterator[IO[str | bytes]], line_selector: str
+#     open_files: Iterator[OPEN_IO], line_selector: str
 # ) -> Iterator[str]:
 #     pass
 
